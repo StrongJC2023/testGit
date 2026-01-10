d82dec0c5d389a3e45b6362014cf571cd724a427
JC <sunjc469947586@163.com>
2026-01-10 23:15:34 +0800

    test_feature_01

---
+++ src/main/java/com/jc/testgit/TestController.java
@@ -0,0 +1,10 @@
+package com.jc.testgit;
+
+/**
+ * @author Strong_JC
+ * @date 2026/1/10 23:12
+ * @description:
+ */
+public class TestController {
+    public static final String WORD_TEST = "word_test";
+}
